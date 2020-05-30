b615b1996b4e6a4f0fe4fde32e9e25cb9626fd63
funsworkings <funsworkings@gmail.com>
2020-05-30 00:13:19 -0400

    Added revolve camera / Revolve camera debug controls

---
+++ butterflowers/Assets/Scripts/uwu/Camera/Instances/RevolveCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Cinemachine;
+
+public class RevolveCamera : GameCamera
+{
+    [SerializeField] Transform target = null;
+
+    CinemachineOrbitalTransposer orbitTransposer = null;
+
+    [Header("Attributes")]
+        [SerializeField] float radius = 1f, height = 0f;
+        [SerializeField] float angle = 0f;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        orbitTransposer = camera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (orbitTransposer == null) return;
+
+        float dt = Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow)) angle += 90f * dt;
+        if (Input.GetKey(KeyCode.RightArrow)) angle -= 90f * dt;
+
+        float angToRad = angle * Mathf.Deg2Rad;
+        Vector3 offset = new Vector3(Mathf.Cos(angToRad)*radius, height, Mathf.Sin(angToRad)*radius);
+
+        orbitTransposer.m_FollowOffset = offset;
+    }
+}
