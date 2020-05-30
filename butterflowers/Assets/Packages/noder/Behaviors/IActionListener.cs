f3e97b93050ddb6220e45ba9fcaa8a56ccce43cc
funsworkings <funsworkings@gmail.com>
2020-05-30 12:38:31 -0400

    Added IActionListener for action nodes

---
+++ butterflowers/Assets/Packages/noder/Behaviors/IActionListener.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Noder.Behaviors {
+
+    public interface IActionListener {
+        void Respond();
+        void RespondWithData(object o);
+    }
+
+}
