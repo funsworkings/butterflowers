4b5591b172fc19ca036967546b0ed1aebeabf6f2
funsworkings <funsworkings@gmail.com>
2020-11-29 09:52:59 -0800

    Scroll to focus/lose focus on objects

--- butterflowers/Assets/Scripts/Objects/Entities/Interactables/Star.cs
+++ butterflowers/Assets/Scripts/Objects/Entities/Interactables/Star.cs
@@ -2,7 +2,4 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Star : Focusable
-{
-
-}
+public class Star : Focusable { }
---
+++ butterflowers/Assets/Scripts/uwu/Extensions/BackgroundMonoBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMonoBehaviour : MonoBehaviour
+{
+    protected bool focused = true;
+    
+    void OnApplicationFocus(bool hasFocus)
+    {
+        focused = hasFocus;
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        focused = !pauseStatus;
+    }
+}
