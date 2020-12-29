bef2bf1d16b1c4af2c37dc6b835bd6bd6a2d47a3
funsworkings <funsworkings@gmail.com>
2020-12-29 09:44:17 -0800

    Basic sequence impl

---
+++ butterflowers/Assets/Scripts/Data/SequenceData.cs
@@ -0,0 +1,11 @@
+﻿using Neue.Reference.Types;
+
+namespace Data
+{
+	[System.Serializable]
+	public class SequenceData
+	{
+		public int index = 0;
+		public Frame[] frames = new Frame[]{};
+	}
+}
--- butterflowers/Assets/Scripts/Objects/Entities/Interactables/Empty/Star.cs
+++ butterflowers/Assets/Scripts/Objects/Entities/Interactables/Empty/Star.cs
@@ -1,5 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Interfaces;
 using UnityEngine;
 
-public class Star : Focusable { }
+public class Star : Focusable, ITooltip
+{
+	public string GetInfo()
+	{
+		return "magic star";
+	}
+}
