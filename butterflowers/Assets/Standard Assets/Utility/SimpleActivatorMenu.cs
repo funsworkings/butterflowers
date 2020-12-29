860b3bd73d8d0ef3ce146de4f2b5bc2d65d10264
funsworkings <funsworkings@gmail.com>
2020-12-29 15:28:05 -0800

    Upgrade proj to 2019.4.7

--- butterflowers/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ butterflowers/Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -6,6 +6,7 @@ namespace UnityStandardAssets.Utility
 {
     public class SimpleActivatorMenu : MonoBehaviour
     {
+        /*
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
         public GUIText camSwitchButton;
@@ -35,5 +36,6 @@ namespace UnityStandardAssets.Utility
             m_CurrentActiveObject = nextactiveobject;
             camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
+        */
     }
 }
