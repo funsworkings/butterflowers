d40c7f1e4740ba6b3f4020ca77f0d72e6d3b018b
Easton <eastonself@gmail.com>
2021-01-12 00:27:49 -0500

    nest shader snapped

---
+++ butterflowers/Assets/GetMousePosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMousePosition : MonoBehaviour
+{
+    public Material lava;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lava.SetVector("_MousePos", Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+}
