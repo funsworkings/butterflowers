d64140ff4c29cba2c11b332d638a6d279e218d2b
funsworkings <funsworkings@gmail.com>
2021-01-17 14:59:08 -0500

    Add burst notifs

---
+++ butterflowers/Assets/Scripts/butterflowersOS/Snippets/Burster.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace butterflowersOS.Snippets
+{
+	public class Burster : MonoBehaviour
+	{
+		[SerializeField] GameObject pr_burst;
+		[SerializeField] float cameraDistance = 1f;
+
+		Camera camera;
+    
+		// Start is called before the first frame update
+		void Start()
+		{
+			camera = Camera.main;
+		}
+
+		public void Burst(Vector3 position)
+		{
+			var worldPosition = camera.ScreenToWorldPoint(new Vector3(position.x, position.y, cameraDistance));
+			Instantiate(pr_burst, worldPosition, pr_burst.transform.rotation);
+		}
+	}
+}
