f7adb00c4cd572b41fede7aa654c591badb84a61
funsworkings <funsworkings@gmail.com>
2021-01-28 10:20:03 -0500

    Import latestegeo

---
+++ butterflowers/Assets/Scripts/butterflowersOS/UI/WelcomeMessage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using butterflowersOS.Core;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+using uwu;
+
+namespace butterflowersOS.UI
+{
+    public class WelcomeMessage : MonoBehaviour
+    {
+        World World;
+        
+        // Properties
+
+        [SerializeField] TMP_Text text;
+
+        // Start is called before the first frame update
+        IEnumerator Start()
+        {
+            World = World.Instance;
+            while (!World.LOAD) yield return null;
+
+            var username = World.username;
+            DisplayUsername(username);
+        }
+
+        void DisplayUsername(string username)
+        {
+            if (string.IsNullOrEmpty(username)) 
+            {
+                text.gameObject.SetActive(false);
+                return;
+            }
+            
+            text.text = username;
+        }
+    }
+}
