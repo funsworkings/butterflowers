e6125b5f674745209198769e12232731128322a1
funsworkings <funsworkings@gmail.com>
2021-02-01 18:46:19 -0500

    Make export notification more prominent!

---
+++ butterflowers/Assets/Scripts/butterflowersOS/UI/Notifications/ExportNotification.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace butterflowersOS.UI.Notifications
+{
+	public class ExportNotification : GenericNotification
+	{
+		// Attributes
+		
+		[SerializeField] AnimationCurve scaleCurve;
+		[SerializeField] float scaleTime = 1f;
+		
+
+		void Start()
+		{
+			transform.localScale = Vector3.zero;
+		}
+
+		protected override void OnInitialize()
+		{
+			transform.localScale = Vector3.zero;
+		}
+
+		protected override void OnUpdate()
+		{
+			float scale = scaleCurve.Evaluate(Mathf.Clamp01(time / scaleTime));
+			transform.localScale = Vector3.one * scale;
+		}
+	}
+}
