e823f2f2bba0d676c8a77904824dcbae878cddd8
Easton <eastonself@gmail.com>
2021-02-01 01:27:00 -0500

    Revert "merged naster"
    This reverts commit 0fec8f003a1055165490abc09f629d31a72442c3, reversing
    changes made to 209477d3dbfa6ff36d45a3e0325bae3812a06733.

--- butterflowers/Assets/Scripts/butterflowersOS/Data/SurveillanceDataDelta.cs
+++ butterflowers/Assets/Scripts/butterflowersOS/Data/SurveillanceDataDelta.cs
@@ -5,35 +5,40 @@ namespace butterflowersOS.Data
 	[System.Serializable]
 	public class SurveillanceDataDelta
 	{
+		public float filesAdded = 0f; // Gluttony, nurture
+		public float filesRemoved = 0f; // Destruction, spontaneity
 		public float discoveries = 0f; // Spontaneity, play
 
 		public float hob = 0f; // Destruction, play
 		public float nestfill = 0f; // Nurture, destruction, gluttony
+		public float cursorspeed = 0f; // Play, spontaneity
+
+		// Rest, spontaneity, play
+		public float volatility = 0f;
 
 		public float beaconsAdded = 0f; // Gluttony, destruction
 		public float beaconsPlanted = 0f; // Nurture, rest
 
 		public float nestKicks = 0f; // Play, rest
 		public float nestSpills = 0f; // Destruction, gluttonyttony
-
-		public float MAX_DELTA;
 		
 		
 		public SurveillanceDataDelta(CompositeSurveillanceData a, CompositeSurveillanceData b)
 		{
-			float MD = 0F;
-			
-			discoveries = Extensions.PercentageDifferenceWithMax(a.Discoveries, b.Discoveries, ref MD);
+			filesAdded = Extensions.PercentageDifference(a.FilesAdded, b.FilesAdded);
+			filesRemoved = Extensions.PercentageDifference(a.FilesRemoved, b.FilesRemoved);
+			discoveries = Extensions.PercentageDifference(a.Discoveries, b.Discoveries);
 
-			hob = Extensions.PercentageDifferenceWithMax(a.AverageHoB, b.AverageHoB, ref MD);
-			nestfill = Extensions.PercentageDifferenceWithMax(a.AverageNestFill, b.AverageNestFill, ref MD);
-			
-			beaconsAdded = Extensions.PercentageDifferenceWithMax(a.BeaconsAdded, b.BeaconsAdded, ref MD);
-			beaconsPlanted = Extensions.PercentageDifferenceWithMax(a.BeaconsPlanted, b.BeaconsPlanted, ref MD);
-			nestKicks = Extensions.PercentageDifferenceWithMax(a.NestKicks, b.NestKicks, ref MD);
-			nestSpills = Extensions.PercentageDifferenceWithMax(a.NestSpills, b.NestSpills, ref MD);
+			hob = Extensions.PercentageDifference(a.AverageHoB, b.AverageHoB);
+			nestfill = Extensions.PercentageDifference(a.AverageNestFill, b.AverageNestFill);
+			cursorspeed = Extensions.PercentageDifference(a.AverageCursorSpeed, b.AverageCursorSpeed);
 
-			MAX_DELTA = MD;
+			volatility = Extensions.PercentageDifference(a.CalculateFocusVolatility(), b.CalculateFocusVolatility());
+
+			beaconsAdded = Extensions.PercentageDifference(a.BeaconsAdded, b.BeaconsAdded);
+			beaconsPlanted = Extensions.PercentageDifference(a.BeaconsPlanted, b.BeaconsPlanted);
+			nestKicks = Extensions.PercentageDifference(a.NestKicks, b.NestKicks);
+			nestSpills = Extensions.PercentageDifference(a.NestSpills, b.NestSpills);
 		}
 	}
 }
