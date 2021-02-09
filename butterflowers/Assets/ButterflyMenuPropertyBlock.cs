63a85cc067fb575fee4720eb8d0455e74ab5b968
funsworkings <funsworkings@gmail.com>
2021-02-09 12:08:50 -0500

    Slow down menu butterflowers

--- butterflowers/Assets/ButterflyMenuPropertyBlock.cs
+++ butterflowers/Assets/ButterflyMenuPropertyBlock.cs
@@ -15,7 +15,7 @@ public class ButterflyMenuPropertyBlock : MonoBehaviour
         propBlock = new MaterialPropertyBlock();
 
         propBlock.SetFloat("_TimeOffset", Random.Range(0f,1f));
-        propBlock.SetFloat("_Speed", Random.Range(0f, 1f));
+        propBlock.SetFloat("_Speed", Random.Range(0f, .067f));
         r.SetPropertyBlock(propBlock);
     }
 
