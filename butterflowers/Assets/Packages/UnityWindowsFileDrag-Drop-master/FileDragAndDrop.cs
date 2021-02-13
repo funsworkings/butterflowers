1921eb5e0ed352d1ccbebac4c06d9b9245815a50
rick gillis <rick@eternal.plus>
2021-02-13 11:01:53 -0500

    Working file drag&drop support on mac

--- butterflowers/Assets/Packages/UnityWindowsFileDrag-Drop-master/FileDragAndDrop.cs
+++ butterflowers/Assets/Packages/UnityWindowsFileDrag-Drop-master/FileDragAndDrop.cs
@@ -9,6 +9,7 @@ using Neue.Agent.Brain.Data;
 using Neue.Utilities;
 using Shibuya24.Utility;
 using uwu.Data;
+using uwu.Extensions;
 using uwu.IO;
 
 
@@ -39,7 +40,22 @@ public class FileDragAndDrop : MonoBehaviour
 
     void OnFiles(string lFiles)
     {
-        Debug.LogError("Did receive files from mac drag & drop => " + lFiles);
+        //Debug.LogError("Did receive files from mac drag & drop => " + lFiles);
+
+        if (!string.IsNullOrEmpty(lFiles))
+        {
+            string[] files = JsonHelper.getJsonArray<string>(lFiles);
+            POINT pt = new POINT(0, 0); // Dummy point for import from drag
+
+            if (files.Length > 0)
+            {
+                var receivers = FindObjectsOfType<MonoBehaviour>().OfType<IFileDragAndDropReceiver>();
+                foreach (IFileDragAndDropReceiver receiver in receivers)
+                {
+                    receiver.ReceiveFiles(new List<string>(files), pt);
+                }
+            }
+        }
     }
 
     void OnFiles(List<string> aFiles, POINT aPos)
