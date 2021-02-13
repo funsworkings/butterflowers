ead8514ce415213be4c2b57789b061f618db5e09
funsworkings <funsworkings@gmail.com>
2021-02-13 18:08:48 -0500

    import latest v1

--- butterflowers/Assets/Packages/UnityWindowsFileDrag-Drop-master/FileDragAndDrop.cs
+++ butterflowers/Assets/Packages/UnityWindowsFileDrag-Drop-master/FileDragAndDrop.cs
@@ -7,7 +7,9 @@ using butterflowersOS;
 using butterflowersOS.Core;
 using Neue.Agent.Brain.Data;
 using Neue.Utilities;
+using Shibuya24.Utility;
 using uwu.Data;
+using uwu.Extensions;
 using uwu.IO;
 
 
@@ -15,13 +17,45 @@ public class FileDragAndDrop : MonoBehaviour
 {
     void OnEnable ()
     {
-        // must be installed on the main thread to get the right thread id.
-        UnityDragAndDropHook.InstallHook();
-        UnityDragAndDropHook.OnDroppedFiles += OnFiles;
+        #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            // must be installed on the main thread to get the right thread id.
+            UnityDragAndDropHook.InstallHook();
+            UnityDragAndDropHook.OnDroppedFiles += OnFiles;
+        
+        #elif UNITY_STANDALONE_OSX
+
+        UniDragAndDrop.onDragAndDropFiles += OnFiles;
+        UniDragAndDrop.Initialize();
+
+        #endif
     }
     void OnDisable()
     {
-        UnityDragAndDropHook.UninstallHook();
+        #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            UnityDragAndDropHook.UninstallHook();
+        #elif UNITY_STANDALONE_OSX
+            UniDragAndDrop.onDragAndDropFiles -= OnFiles;
+        #endif
+    }
+
+    void OnFiles(string lFiles)
+    {
+        //Debug.LogError("Did receive files from mac drag & drop => " + lFiles);
+
+        if (!string.IsNullOrEmpty(lFiles))
+        {
+            string[] files = JsonHelper.getJsonArray<string>(lFiles);
+            POINT pt = new POINT(0, 0); // Dummy point for import from drag
+
+            if (files.Length > 0)
+            {
+                var receivers = FindObjectsOfType<MonoBehaviour>().OfType<IFileDragAndDropReceiver>();
+                foreach (IFileDragAndDropReceiver receiver in receivers)
+                {
+                    receiver.ReceiveFiles(new List<string>(files), pt);
+                }
+            }
+        }
     }
 
     void OnFiles(List<string> aFiles, POINT aPos)
--- butterflowers/Assets/Scripts/uwu/Snippets/CustomCursor.cs
+++ butterflowers/Assets/Scripts/uwu/Snippets/CustomCursor.cs
@@ -18,6 +18,16 @@ namespace uwu.Snippets
 
 		[SerializeField] Setting[] settings;
 
+		private void OnApplicationFocus(bool hasFocus)
+		{ 
+			//NativeCursor.visible = hasFocus;
+		}
+
+		private void OnApplicationPause(bool pauseStatus)
+		{
+			//NativeCursor.visible = !pauseStatus;
+		}
+
 		// Update is called once per frame
 		void Update()
 		{
@@ -27,10 +37,6 @@ namespace uwu.Snippets
 				var tex = setting.icon;
 
 				var mode = CursorMode.Auto;
-
-#if UNITY_STANDALONE_OSX && !UNITY_EDITOR
-                mode = CursorMode.ForceSoftware;
-#endif
 
 				var visible = NativeCursor.visible = (tex != null);
 				if(visible)
