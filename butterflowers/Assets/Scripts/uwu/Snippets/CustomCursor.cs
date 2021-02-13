bec3bcf164d626bdf757e3b4717b62e9b3c1c9e5
rick gillis <rick@eternal.plus>
2021-02-13 11:09:51 -0500

    Hide cursor if application is not in focus

--- butterflowers/Assets/Scripts/uwu/Snippets/CustomCursor.cs
+++ butterflowers/Assets/Scripts/uwu/Snippets/CustomCursor.cs
@@ -18,6 +18,16 @@ namespace uwu.Snippets
 
 		[SerializeField] Setting[] settings;
 
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			NativeCursor.visible = hasFocus;
+		}
+
+		private void OnApplicationPause(bool pauseStatus)
+		{
+			NativeCursor.visible = !pauseStatus;
+		}
+
 		// Update is called once per frame
 		void Update()
 		{
