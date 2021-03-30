da5748b47d86e2ca2bc43534cd5df274ab346551
funsworkings <funsworkings@gmail.com>
2021-03-29 23:01:30 -0400

    Add fps counter

---
+++ butterflowers/Assets/Scripts/butterflowersOS/Miscellaneous/FramesPerSecond.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace butterflowersOS.Miscellaneous
+{
+	public class FramesPerSecond : MonoBehaviour
+	{
+		public int FPS { get; set; }
+
+		TMP_Text textField;
+
+		void Awake()
+		{
+			textField = GetComponent<TMP_Text>();
+		}
+
+		void Update()
+		{
+			FPS = (int)(1f / Time.unscaledDeltaTime);
+			textField.text = FPS + "";
+		}
+	}
+}
