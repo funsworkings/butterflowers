eb800bca4ce634a9c7530a1132383a8b601d7626
funsworkings <funsworkings@gmail.com>
2021-04-17 10:31:13 -0400

    Adjust EOD summary panel!

--- butterflowers/Assets/Scripts/___Deprecated/GradingManager.cs
+++ butterflowers/Assets/Scripts/___Deprecated/GradingManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Linq;
+﻿using System.Collections;
 using butterflowersOS.Core;
 using butterflowersOS.Data;
 using butterflowersOS.Presets;
@@ -11,7 +9,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using uwu.UI.Behaviors.Visibility;
 
-namespace Objects.Managers
+namespace butterflowersOS.Objects.Managers
 {
 	public class GradingManager : MonoBehaviour
 	{
@@ -22,19 +20,7 @@ namespace Objects.Managers
 		// Properties
 
 		[HideInInspector] public bool inprogress = false;
-
-		[SerializeField] WorldPreset Preset;
-		[SerializeField] Animator animator;
-		
-		[SerializeField] TMP_Text score;
-
-		[SerializeField] Transform scoreItemParent;
 		[SerializeField] SummaryDeck deck = null;
-
-		[SerializeField] ToggleScale scoreScaler;
-
-		[SerializeField] Image strokeImage;
-		[SerializeField] Animation scoreDropdownAnimation;
 
 
 		#region Monobehaviour callbacks
@@ -62,19 +48,13 @@ namespace Objects.Managers
 
 			inprogress = true;
 			StartCoroutine("DisplayDeck");
-
-			/*****/
-			//char grade = CalculateGrade(log, compositeLog);
-			//score.text = grade.ToString();
-			/*****/
 		}
 
 		IEnumerator DisplayDeck()
 		{
 			deck.Close(immediate:true);
 			yield return new WaitForEndOfFrame();
-			
-			deck.Drop();
+
 			yield return new WaitForSecondsRealtime(1f);
 			deck.Open();
 		}
@@ -106,64 +86,6 @@ namespace Objects.Managers
 			card.HideScore();
 		}
 
-		#endregion
-		
-		#region Scoring
-
-		public char CalculateGrade(CompositeSurveillanceData current, CompositeSurveillanceData composite)
-		{
-			/*
-			float fileadd = CalculatePercentageIncrease(composite.filesAdded, current.filesAdded);
-			float fileremove = -CalculatePercentageIncrease(composite.filesRemoved, current.filesRemoved);
-			float discovery = CalculatePercentageIncrease(composite.discoveries, current.discoveries);
-
-			float beaconadd = CalculatePercentageIncrease(composite.beaconsAdded, current.beaconsAdded);
-			float beaconplant = CalculatePercentageIncrease(composite.beaconsPlanted, current.beaconsPlanted);
-
-			float nestkick = CalculatePercentageIncrease(composite.nestKicks, current.nestKicks);
-			float nestspill = CalculatePercentageIncrease(composite.nestSpills, current.nestSpills);
-			
-			
-			scores = new float[]{ fileadd, fileremove, discovery, beaconadd, beaconplant, nestkick, nestspill };
-			float averagePercentIncrease = scores.Average();
-
-			if (averagePercentIncrease >= .5f)
-				return 'A';
-			else if (averagePercentIncrease >= .3f)
-				return 'B';
-			else if (averagePercentIncrease >= .125f)
-				return 'C';
-			else if (averagePercentIncrease >= 0f)
-				return 'D';
-			else
-				return 'F';
-				*/
-
-			return 'A';
-		}
-
-		float CalculatePercentageIncrease(float a, float b)
-		{
-			if (a == 0f)
-				a = .001f; // Turn into small value
-			
-			return Mathf.Clamp((b - a)/a, -1f, 1f);
-		}
-		
-		#endregion
-		
-		#region Deck callbacks
-
-		void DeckDidOpen()
-		{
-			
-		}
-		
-		void DeckDidClose()
-		{
-			
-		}
-		
 		#endregion
 	}
 }
---
+++ butterflowers/Assets/Scripts/butterflowersOS/UI/Summary Panels/HighlightsPanel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using butterflowersOS.Core;
+using butterflowersOS.Data;
+using butterflowersOS.UI.Summary_Cards;
+using UnityEngine;
+
+namespace butterflowersOS.UI.Summary_Panels
+{
+	public class HighlightsPanel : SummaryPanel
+	{
+		// Properties
+
+		SummaryDeck deck;
+		Surveillance Surveillance = null;
+
+		protected override void Start()
+		{
+			base.Start();
+			
+			deck = GetComponentInChildren<SummaryDeck>();
+			Surveillance = FindObjectOfType<Surveillance>();
+		}
+
+		protected override void OnShown()
+		{
+			SendHighlightsToCards();
+		}
+
+		protected override void OnHidden()
+		{
+			StopCoroutine("DisplayDeck");
+		}
+
+		void SendHighlightsToCards()
+		{
+			var log = new CompositeSurveillanceData(Surveillance.activeLog);
+			var compositeLog = Surveillance.CreateCompositeAverageLog();
+			
+			foreach (SummaryCard card in deck.Items) 
+			{
+				if (!(card is PhotoOfTheDay)) 
+				{
+					card.ShowScore(compositeLog, log);
+				}
+			}
+
+			StartCoroutine("DisplayDeck");
+		}
+		
+
+		IEnumerator DisplayDeck()
+		{
+			deck.Close(immediate:true);
+			yield return new WaitForEndOfFrame();
+			
+			deck.Open();
+		}
+	}
+}
