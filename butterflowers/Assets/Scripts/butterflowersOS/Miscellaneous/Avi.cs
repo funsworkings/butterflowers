729308356e811d83d58c3af7958d7d9dcd8ff5ba
funsworkings <funsworkings@gmail.com>
2021-04-29 08:52:49 -0400

    bboing hit

---
+++ butterflowers/Assets/Scripts/butterflowersOS/Miscellaneous/Avi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace butterflowersOS.Miscellaneous
+{
+	public class Avi : MonoBehaviour
+	{
+		
+		// Properties
+
+		[SerializeField] GameObject hat, glasses;
+
+		void Start()
+		{
+			Hide();
+		}
+
+		public void Show(int layer)
+		{
+			hat.SetActive(true);
+			glasses.SetActive(true);
+
+			hat.layer = glasses.layer = layer;
+		}
+
+		public void Hide()
+		{
+			hat.SetActive(false);
+			glasses.SetActive(false);
+		}
+	}
+}
