2e86f6834d53abdecbd3a45b016dd2c07834c39b
Rick Gillis <funsworkings@gmail.com>
2022-07-10 18:08:06 -0400

    Import fastblur / Import opencv4unity

--- butterflowers/Assets/Standard Assets/Utility/AutoMoveAndRotate.cs
+++ butterflowers/Assets/Standard Assets/Utility/AutoMoveAndRotate.cs
@@ -1,33 +1,44 @@
 using System;
 using UnityEngine;
 
-namespace UnityStandardAssets.Utility
+namespace LeTai.Asset.TranslucentImage.Demo
 {
     public class AutoMoveAndRotate : MonoBehaviour
     {
         public Vector3andSpace moveUnitsPerSecond;
         public Vector3andSpace rotateDegreesPerSecond;
-        public bool ignoreTimescale;
-        private float m_LastRealTime;
+        public bool            ignoreTimescale, lateUpdate;
+        float                  m_LastRealTime;
 
 
-        private void Start()
+        void Start()
         {
             m_LastRealTime = Time.realtimeSinceStartup;
         }
 
+        void Update()
+        {
+            if (!lateUpdate)
+                DoWork();
+        }
 
-        // Update is called once per frame
-        private void Update()
+        void LateUpdate()
+        {
+            if (lateUpdate)
+                DoWork();
+        }
+
+        void DoWork()
         {
             float deltaTime = Time.deltaTime;
             if (ignoreTimescale)
             {
-                deltaTime = (Time.realtimeSinceStartup - m_LastRealTime);
+                deltaTime      = (Time.realtimeSinceStartup - m_LastRealTime);
                 m_LastRealTime = Time.realtimeSinceStartup;
             }
-            transform.Translate(moveUnitsPerSecond.value*deltaTime, moveUnitsPerSecond.space);
-            transform.Rotate(rotateDegreesPerSecond.value*deltaTime, moveUnitsPerSecond.space);
+
+            transform.Translate(moveUnitsPerSecond.value  * deltaTime, moveUnitsPerSecond.space);
+            transform.Rotate(rotateDegreesPerSecond.value * deltaTime, moveUnitsPerSecond.space);
         }
 
 
@@ -35,7 +46,7 @@ namespace UnityStandardAssets.Utility
         public class Vector3andSpace
         {
             public Vector3 value;
-            public Space space = Space.Self;
+            public Space   space = Space.Self;
         }
     }
 }
