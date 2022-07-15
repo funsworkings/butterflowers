f2f1c8672ae94ee75da4c0018071890785646878
Rick Gillis <funsworkings@gmail.com>
2022-07-14 23:06:40 -0400

    show+hide (key 'D') for debug menu

--- butterflowers/Assets/Scripts/live_simulation/WundervilMenu.cs
+++ butterflowers/Assets/Scripts/live_simulation/WundervilMenu.cs
@@ -10,19 +10,52 @@ namespace live_simulation
     public class WundervilMenu : MonoBehaviour, IBridgeUtilListener
     {
         // Properties
+
+        private CanvasGroup _canvasGroup;
         
         [SerializeField] private TMP_Text _currentFrame, _currentEyeAction, _currentEyeState, _bpm, _actionStack;
         [SerializeField] private Button _restartButton;
+        [SerializeField] private bool visible = false;
 
         public BridgeUtil _Util { get; set; } = null;
         public Action<float, float> OnBeat => null;
 
+        private void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
         void Start()
         {
             BridgeUtil.onUpdateEyeActionStack += UpdateActionStack;
-            
             _restartButton.onClick.AddListener(RestartSimulation);
+            
+            if(visible) Show();
+            else Hide();
         }
+        
+        private void OnDestroy()
+        {
+            BridgeUtil.onUpdateEyeActionStack -= UpdateActionStack;
+        }
+        
+        #region Show/hide
+
+        void Show()
+        {
+            _canvasGroup.alpha = 1f;
+            _canvasGroup.interactable = _canvasGroup.blocksRaycasts = true;
+            visible = true;
+        }
+
+        void Hide()
+        {
+            _canvasGroup.alpha = 0f;
+            _canvasGroup.interactable = _canvasGroup.blocksRaycasts = false;
+            visible = false;
+        }
+        
+        #endregion
 
         void UpdateActionStack(EVENTCODE[] actions)
         {
@@ -46,17 +79,18 @@ namespace live_simulation
 
         void Update()
         {
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                if (visible) Hide();
+                else Show();
+            }
+            
             if (_Util == null) return;
 
             _currentFrame.text = $"frame: {_Util.CurrentFrame}";
             _currentEyeState.text = $"state <eye>: {_Util.CurrentEyeState}";
             _currentEyeAction.text = $"action: {((_Util.CurrentAction.HasValue) ? _Util.CurrentAction.Value.ToString() : "NONE")}";
             _bpm.text = $"BPM: {_Util.BPM}";
-        }
-
-        private void OnDestroy()
-        {
-            BridgeUtil.onUpdateEyeActionStack -= UpdateActionStack;
         }
 
         #region Utils
